5ebe40d0c6f92d3213afc49913a9a528eb09dc5a
Rika Sumitomo <muu_and_sin@yahoo.co.jp>
2019-04-10 20:16:55 +0900

    流す音楽を設定するメソッドを書いた それぞれのボタンを押した時に音楽を流すようにした

--- Music/ViewController.swift
+++ Music/ViewController.swift
@@ -14,20 +14,32 @@ class ViewController: UIViewController {
     var audioPlayer: AVAudioPlayer!
 
     @IBAction func natsuPlayButton(){
-        
+        setAudioPlayer(soundName: "natsu", type: "mp3")
+        audioPlayer.play()
     }
     @IBAction func amePlayButotn(){
-
+        setAudioPlayer(soundName: "ame", type: "mp3")
+        audioPlayer.play()
     }
     @IBAction func akiPlayButton(){
-        
+        setAudioPlayer(soundName: "aki", type: "mp3")
+        audioPlayer.play()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    func setAudioPlayer(soundName: String, type: String) {
+        let soundFilePath = Bundle.main.path(forResource: soundName, ofType: type)!
+        let fileURL = URL(fileURLWithPath: soundFilePath)
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: fileURL)
+        } catch {
+            print("音楽ファイルが読み込めませんでした")
+        }
+    }
 
 }
 
